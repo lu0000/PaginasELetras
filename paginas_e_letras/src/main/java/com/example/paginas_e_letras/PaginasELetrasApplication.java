d3837685b7514d4bd7241db8c3fa22661effcd52
Luana <114164271+lu0000@users.noreply.github.com>
2024-08-24 11:20:44 -0300

    Add files via upload

---
+++ paginas_e_letras/src/main/java/com/example/paginas_e_letras/PaginasELetrasApplication.java
@@ -0,0 +1,13 @@
+package com.example.paginas_e_letras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginasELetrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginasELetrasApplication.class, args);
+	}
+
+}
